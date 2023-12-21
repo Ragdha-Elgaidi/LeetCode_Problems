57699cc51c7b5d452ddbe22e1c368f4c26a1cee9
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-12-21 16:49:08 +0200

    Create 2706. Buy Two Chocolates.cpp

---
+++ Easy/2706. Buy Two Chocolates.cpp
@@ -0,0 +1,15 @@
+class Solution 
+{
+public:
+    int buyChoco(vector<int>& prices, int money) 
+    {
+        sort(prices.begin(), prices.end());
+        int minCost = prices[0] + prices[1];
+
+        if (minCost <= money) 
+        {
+            return money - minCost;
+        }
+        return money;
+    }
+};
